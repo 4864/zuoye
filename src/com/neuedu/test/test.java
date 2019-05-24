f780fcb3d31ef52feb8c335bdd2a06d197128987
4864 <1670837554@qq.com>
2019-05-24 19:05:07 +0800

    y6

---
+++ src/com/neuedu/test/test.java
@@ -0,0 +1,15 @@
+package com.neuedu.test;
+
+public class test {
+    public static void main(String[] args) {
+        int a=68;
+        if (a>=90){
+            System.out.println("A");
+        }else if (a>=60&&a<90){
+            System.out.println("B");
+        }else{
+            System.out.println("C");
+        }
+
+    }
+}
